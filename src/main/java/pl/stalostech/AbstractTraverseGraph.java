283c39db67589e1029e91ef0d334163063adb4c1
bwysocki <wysockibartosz@gmail.com>
2014-08-19 09:16:22 +0200

    Cypher change.

--- src/main/java/pl/stalostech/AbstractTraverseGraph.java
+++ src/main/java/pl/stalostech/AbstractTraverseGraph.java
@@ -12,7 +12,7 @@ import org.neo4j.graphdb.Transaction;
 
 public abstract class AbstractTraverseGraph {
 
-    protected String GET_NODE_BY_INDEX = "MATCH (n:Node) WHERE n.name={name} RETURN n";
+    protected String GET_NODE_BY_INDEX = "MATCH (n:Node) WHERE n.name={name} RETURN n AS nodeByIndex";
 
     /**
      * Returns string representing the nodes visited by algorithm.
@@ -64,7 +64,7 @@ public abstract class AbstractTraverseGraph {
         ExecutionResult result = executionEngine.execute(GET_NODE_BY_INDEX, params);
 
         //add first node to the queue
-        return (Node) result.iterator().next().get("n");
+        return (Node) result.iterator().next().get("nodeByIndex");
     }
 
 }
